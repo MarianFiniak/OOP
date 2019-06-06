8fce8dba1b471fd2e05f9106d5f301e05df81c01
MarianFiniak <43724557+MarianFiniak@users.noreply.github.com>
2019-06-06 22:53:21 +0300

    lab1

---
+++ Editor.java
@@ -0,0 +1,95 @@
+package demo;
+
+public class Editor
+{
+    static StringBuilder sentence;
+
+
+    static void settings(String kros)
+    {
+        sentence = new StringBuilder(kros);
+    }
+
+    static void tut()
+    {
+        for(int i = 0; i < sentence.length(); i++)
+        {
+            System.out.print(sentence.charAt(i));
+            if(sentence.charAt(i) == '.')
+            {
+                System.out.println();
+                i++;
+            }
+        }
+
+        System.out.println();
+    }
+
+    static void WordChecker(int debug)
+    {
+        int conter,mount;
+        int indexOfSpace = -1;
+        int nextIndexOfSpace = 0;
+        int lastIndexOfSpace = sentence.lastIndexOf(" ");
+        String substr1;
+        int l;
+
+
+        do
+
+        {
+            conter = 0;
+            mount = 0;
+            nextIndexOfSpace = sentence.indexOf(" ",indexOfSpace+1);
+
+            if(nextIndexOfSpace == lastIndexOfSpace)
+            {
+
+                if(sentence.charAt(lastIndexOfSpace+1) == sentence.charAt(sentence.length()-2))
+                {
+                    System.out.println("WORD IS CORRECT:");
+                    System.out.println(sentence.substring(lastIndexOfSpace+1,sentence.length()-1));
+                }
+                else if(debug == 1)
+                {
+                    System.out.println("WORD IS INCORRECT:");
+                    System.out.println(sentence.substring(lastIndexOfSpace+1,sentence.length()-1));
+                }
+            }
+            if (nextIndexOfSpace ==-1) break;
+            substr1 = sentence.substring(indexOfSpace+1,nextIndexOfSpace);
+            indexOfSpace = nextIndexOfSpace;
+            // checking words algorithm
+            l = substr1.length();
+            if(substr1.charAt(l-1) == '.')
+            {
+                if(substr1.charAt(0) == substr1.charAt(l-2))
+                {
+                    mount = 1;
+                    System.out.println("WORD IS CORRECT");
+                    System.out.println(substr1.substring(0,l-1));
+                }
+                else if(debug == 1)
+                {
+                    conter = 1;
+                    System.out.println("WORD IS INCORRECT:");
+                    System.out.println(substr1.substring(0,l-1));
+                }
+            }
+            if(substr1.charAt(0) == substr1.charAt(l-1))
+            {
+                System.out.println("WORD IS CORRECT");
+                System.out.println(substr1);
+            }
+            else if(debug == 1 && conter == 0 && mount == 0)
+            {
+
+                System.out.println("WORD IS INCORRECT:");
+                System.out.println(substr1);
+            }
+        }
+        while(indexOfSpace != -1);
+
+    }
+
+}
--- Main.java
+++ Main.java
@@ -1,180 +1,63 @@
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
+package demo;
+
 import java.util.Scanner;
 
-public class Main {
-    //static int i = 0;
-    public static void main(String[] args) throws IOException, TransformerException, ParserConfigurationException, SAXException, ClassNotFoundException {
-        ConstructorsForXML constructorsForXML = new ConstructorsForXML();
-        Scanner in = new Scanner(System.in);
-        String FILENAME = "file1.xml";
-        String s4 ;
+public class Main
+{
+    public static void main(String[] args)
+    {
 
-        ArrayList<Demain> train = new ArrayList<>();
-
-        Filego file = new Filego();
-        String s3;
-        int M=0;
-
+        int debug = 0;
         if (args.length > 0) {
-            if (args[0].equals("-auto")) {
-                train = constructorsForXML.read("file.xml");
-                Collections.sort(train);
-                String[] strings = file.make_info(train);
-                for (String s : strings) {
-                    System.out.println(s);
+
+
+            if (args[0].equals("-d") || (args.length > 1 && args[1].equals("-d"))) {
+                debug = 1;
+                System.out.println("Вітаємо в режимі 'debug' ");
+            }
+            if (args[0].equals("-h") || (args.length > 1 && args[1].equals("-h")))
+                System.out.println(
+                        "Виконав: Мар'ян Фіняк, КН-108 \n\n"
+                                + "Мета:\n"
+                                + "Ввести текст.Текст розбити на речення \n"
+                                + "У словах зробити перевірку\n"
+                                + "Якщо перша та остання літери співпали,вивести слово\n"
+                                + "Після кожного слова потрібно поставити пробіл\n"
+                                + "Після кінця речення потрібно поставити крапку і пробіл.В кінці введеного тексту поставте крапку\n\n"
+                                + "Команди меню:\n"
+                                + "Для виводу розділенного тексту введіть \"l\" \n"
+                                + "Для виводу слів введіть \"w\" \n"
+                                + "Для вводу нового тексту \"n\"");
+        }
+        System.out.println("\n\nВведіть рядок для редактування:");
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        Editor.settings(str);
+        System.out.print("Наступна команда: ");
+        String k = in.nextLine();
+        for(;;System.out.print("Наступна команда: "),k = in.nextLine())
+        {
+            switch(k.charAt(0)){
+                case 'n': {
+                    System.out.println("Новий рядок: ");
+                    str = in.nextLine();
+                    Editor.settings(str);
+                    break;
+                }
+                case 'l':
+                    Editor.tut();
+                    break;
+                case 'w': {
+                    Editor.WordChecker(debug);
+                    break;
+                }
+                default: {
+                    System.out.println("Вихід!");
+                    return;
                 }
             }
-        } else {
-            do {
-                System.out.println("*Записати нову інформацію (1)\n" +
-                        "*Добавити інформацію (2) \n" +
-                        "*Переглянути (3)\n" +
-                        "*Зберегти (4)\n" +
-                        "*Видалити (5)\n" +
-                        "*Сортувати (6)\n" +
-                        "*Вийти (7)");
-                System.out.print("Ваша команда : ");
-                s4 = in.nextLine();
-                System.out.println();
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                if (s4.equals("1")) {
-                    System.out.println("Введіть кількість маршрутів: ");
-                    M = in.nextInt();
-                    for (int k = 0; k < M; k++) {
-                        Demain demain = new Demain();
-                        train.add(demain);
-
-                    }
-
-                }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                if (s4.equals("2")) {
-                    System.out.println("Ви хочете вибрати файл, куди добавити інформації?(Т/Н):");
-                    s3 = in.nextLine();
-                    if (s3.equals("Т")) {
-                        FILENAME = file.doFile();
-                    }
-                    train.clear();
-                    train = constructorsForXML.read(FILENAME);
-
-                    System.out.println("Введіть кількість маршрутів: ");
-                    int K = in.nextInt();
-
-
-                    for (int k = 0; k < K; k++) {
-                        Demain demain = new Demain();
-                        train.add(demain);
-                        M++;
-                    }
-                }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                if (s4.equals("3")) {
-
-                    System.out.println("Ви хочете вибрати файл, звідки будете дивитися інформацію?(Т/Н):");
-                    s3 = in.nextLine();
-
-                    if (s3.equals("Т")) {
-                        FILENAME = file.doFile();
-                    }
-                    System.out.print("Дивитися з файлу .xml чи .out(1/2): ");
-                    s3=in.nextLine();
-                    ArrayList<Demain> train2 = new ArrayList<>();
-                    if(s3.equals("1")) {
-                        train2.clear();
-                        train2 = constructorsForXML.read(FILENAME);
-                    }
-                    else {
-                        FileInputStream fis = new FileInputStream("temp.out");
-                        ObjectInputStream oin = new ObjectInputStream(fis);
-                        train2 = (ArrayList<Demain>) oin.readObject();
-                    }
-                   // System.out.println(train2.get(0).getNumberFlight());
-                    String[] strings = file.make_info(train2);
-                    for (String s : strings) {
-                        System.out.println(s);
-                    }
-                    //System.out.println(train2.size());
-
-                }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                if (s4.equals("4")) {
-                    System.out.println("Ви хочете вибрати файл?(Т/Н):");
-                    s3 = in.nextLine();
-                    if (s3.equals("Т")) {
-                        FILENAME = file.doFile();
-                    }
-                    System.out.print("Зберегти в .xml чи .out файл?(1/2): ");
-                    s3 = in.nextLine();
-                    if (s3.equals("1")) {
-                        constructorsForXML.WriteParamXML(train, FILENAME);
-                    } else {
-                        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("temp.out"));
-                        oos.writeObject(train);
-                        oos.flush();
-                        oos.close();
-                    }
-                }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                if (s4.equals("5")) {
-                    System.out.print("Видалити інформацію з файлу чи з контейнера?(ф/к): ");
-                    s3 = in.nextLine();
-                    if (s3.equals("ф")) {
-                        System.out.println("Ви хочете вибрати файл?(Т/Н):");
-                        s3 = in.nextLine();
-                        if (s3.equals("Т")) {
-                            FILENAME = file.doFile();
-                        }
-                        System.out.print("Файл .xml чи .out?(1/2): ");
-                        s3=in.nextLine();
-                        if (s3.equals("1")) {
-                            train = constructorsForXML.read(FILENAME);
-                        }
-                        else {
-                            FileInputStream fis = new FileInputStream("temp.out");
-                            ObjectInputStream oin = new ObjectInputStream(fis);
-                            train = (ArrayList<Demain>) oin.readObject();
-                        }
-                    }
-                    System.out.println("Видалити всю інформацію?(Т/Н): ");
-                    s3 = in.nextLine();
-                    if (s3.equals("Т")) {
-                        train.clear();
-                        M=0;
-                    } else {
-                        System.out.print("Введіть індекс елемента який ви хочете видалити: ");
-                        int i = in.nextInt();
-                        train.remove(i);
-                        M--;
-                    }
-                }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-             if(s4.equals("6")){
-                 System.out.print("Файл .xml чи .out?(1/2): ");
-                 s3=in.nextLine();
-                 if (s3.equals("1")) {
-                     train = constructorsForXML.read(FILENAME);
-                 }
-                 else {
-                     FileInputStream fis = new FileInputStream("temp.out");
-                     ObjectInputStream oin = new ObjectInputStream(fis);
-                     train = (ArrayList<Demain>) oin.readObject();
-                 }
-                 Collections.sort(train);
-             }
-
-            } while (!s4.equals("7"));
         }
     }
+
 }
-
-
-
-
-
-
